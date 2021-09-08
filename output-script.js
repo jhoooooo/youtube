fabadf1db7283b73c231a486813f6c119e16237f
metadotmy <metadotmy@gmail.com>
2021-09-08 13:02:16 +0800

    Added youtube embed.

--- output-script.js
+++ output-script.js
@@ -26,13 +26,36 @@ async function getJson(url) {
 }
 
 async function main() {
-
     jsondata2 = await getJson(fullUrl)
     console.log(jsondata2);
     console.log('Video ID: ', jsondata2.items[0].id.videoId);
     console.log('HD Thumbnail URL: ', jsondata2.items[0].snippet.thumbnails.high.url);
     console.log('Max Res Thumbnail: ', 'https://img.youtube.com/vi/' + jsondata2.items[0].id.videoId + '/maxresdefault.jpg')
+}
 
+var tag = document.createElement('script');
+tag.src = 'https://www.youtube.com/iframe_api';
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+var player;
+async function onYouTubeIframeAPIReady() {
+    jsondata2 = await getJson(fullUrl)
+    video = new YT.Player('player', {
+        height: 360,
+        width: 640,
+        videoId: jsondata2.items[0].id.videoId,
+        playerVars: {
+            'fs': 0,
+            'modestbranding': 1,
+            'playsinline': 1,
+            'rel': 0,
+            'autoplay': 1,
+            'controls': 0,
+            'autohide': 1,
+            'mute': 0,
+            'origin': 'https://jhoooooo.github.io/'
+            }
+    });
 }
 
 main();
